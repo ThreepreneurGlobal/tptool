e8c968fc661ad960466f4895cf7245640ab319c9
vaibhavallewar3 <120300272+vaibhavallewar3@users.noreply.github.com>
2024-02-02 13:06:22 +0530

    Delete Util directory

--- Util/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('collage-placement', 'root', '@Ritesh123', {
-    dialect: 'mysql',
-    host: 'localhost'
-}); 
-
-module.exports = sequelize;  
