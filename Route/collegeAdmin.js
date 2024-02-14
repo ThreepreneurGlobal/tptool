b117419aedb27e10518e8c5341a6e74e8c6e4872
riteshsute <suteritesh@gmail.com>
2024-02-14 16:08:32 +0530

    change in routes

---
+++ Route/collegeAdmin.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const authenticateRole  = require('../middelware/auth'); 
+const adminController = require('../Controller/studentDataController'); 
+
+const router = express.Router();
+
+router.post('/api/v1/org/upload-data', authenticateRole(['admin']), adminController.uploadExcel);
+router.post('/api/v1/org/upload-form-data', authenticateRole(['admin']), adminController.UploadIndivisualStudent);
+router.get('/api/v1/org/view-uploaded-data', authenticateRole(['admin']), adminController.viewUploadedData);
+router.get('/api/v1/org/export-data-in-excel', authenticateRole(['admin']), adminController.exportDataToExcel);
+router.put('/api/v1/org/update-college-details', authenticateRole([ 'admin']), adminController.updateCollegeDetails);
+router.get('/api/v1/org/search-users-by-name', authenticateRole(['admin']), adminController.searchUsersByName);
+router.post('/api/v1/org/post-recruiter', authenticateRole(['admin']), adminController.postRecruiter);
+router.get('/api/v1/org/display-recruiters', authenticateRole(['admin']), adminController.displayRecruiter);
+router.put('/api/v1/org/delete-student/:studentId', authenticateRole(['admin']), adminController.deleteStudent);
+router.put('/api/v1/org/delete-recruiter/:recruiterId', authenticateRole(['admin']), adminController.deleteRecruiter);
+
+module.exports = router;
--- Route/superUserRoute.js
+++ Route/superUserRoute.js
@@ -3,15 +3,15 @@ const path = require('path');
 const express = require('express');
 const router = express.Router();
 const authenticateRole  = require('../middelware/auth');
-const collegeController = require('../Controller/collageController');
+const collegeController = require('../Controller/collegeController');
 const loginSuperUserController = require('../Controller/superUserAdminController');
 
-router.post('/api/v1/collage/onboardCollege', authenticateRole([ "superUser" ]), collegeController.onboardCollege);
+router.post('/api/v1/college/onboard-college', authenticateRole([ "superUser" ]), collegeController.onboardCollege);
 
-router.post('/api/v1/collage/saveCollageDetails', authenticateRole([ "superUser" ]), collegeController.collageDetails);
+router.post('/api/v1/college/save-college-details', authenticateRole([ "superUser" ]), collegeController.collegeDetails);
 
-router.get('/api/v1/collage/colleges', authenticateRole([ "superUser" ]), collegeController.getColleges);
+router.get('/api/v1/college/colleges', authenticateRole([ "superUser" ]), collegeController.getColleges);
 
-router.post('/api/v1/user/loginSuper', loginSuperUserController.loginSuperUser); 
+router.post('/api/v1/user/login-super', loginSuperUserController.loginSuperUser); 
 
 module.exports = router;   
