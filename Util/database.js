c42d03997c729b21b181a5303cf131057f7272bd
riteshsute <suteritesh@gmail.com>
2024-02-20 15:50:14 +0530

    upload with env cred

--- Util/database.js
+++ Util/database.js
@@ -1,19 +1,19 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize({
-    database: "college_project", 
-    username: "root", 
-    password: "@Ritesh123",
-    host: "localhost",
-    dialect: 'mysql',
-    dialectModule: require("mysql2"),
-    benchmark: true
-}); 
-
-// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+// const sequelize = new Sequelize({
+//     database: "college_project", 
+//     username: "root", 
+//     password: "@Ritesh123",
+//     host: "localhost",
 //     dialect: 'mysql',
-//     host: process.env.DB_HOST
-// });
+//     dialectModule: require("mysql2"),
+//     benchmark: true
+// }); 
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    dialect: 'mysql',
+    host: process.env.DB_HOST
+});
 
 module.exports = sequelize;  
 
