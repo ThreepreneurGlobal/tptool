6fd56e149c5b7896854f74fb6193226509ff9eef
vaibhavallewar3 <120300272+vaibhavallewar3@users.noreply.github.com>
2024-09-25 17:19:21 +0530

    Update token.js

--- utils/token.js
+++ utils/token.js
@@ -3,8 +3,7 @@ const sendToken = (user, statusCode, resp) => {
     const token = user.getJWToken();
 
     resp.cookie('token', token, {
-        expires: new Date(Date.now() + process.env.COOKIE_EXP * 24 * 60 * 60 * 1000),
-        httpOnly: true, sameSite: 'None', secure: true
+        httpOnly: true, sameSite: 'Lax', secure: true, path: '/', maxAge: process.env.COOKIE_EXP * 24 * 60 * 60 * 1000,
     })
         .status(statusCode).json({ success: true, role: user.role, message: `Welcome ${user?.name?.toUpperCase()}...` });
     // .json({ success: true, role: user.role, token });
