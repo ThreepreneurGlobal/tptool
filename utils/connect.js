1e4ff0fb2d6f5438f6e77f5db624018814bc3dce
vaibhav <vaibhavallewar3@gmail.com>
2024-10-23 11:09:57 +0530

    changes logging method in connect file.

--- utils/connect.js
+++ utils/connect.js
@@ -3,10 +3,7 @@ const { Sequelize } = require("sequelize");
 const Connect = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
     dialect: "mysql",
-    // logging: (msg)=>{
-    //     console.log(msg);
-    // },
-    logging: true,
+    logging: false,
 });
 
 try {
