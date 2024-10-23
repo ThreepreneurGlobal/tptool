4e5a4b217af74b692ec23296caf98c039f7a28a7
vaibhav <vaibhavallewar3@gmail.com>
2024-10-23 11:30:19 +0530

    change in cookie setup in token file.

--- utils/token.js
+++ utils/token.js
@@ -3,11 +3,10 @@ const sendToken = (user, statusCode, resp) => {
     const token = user.getJWToken();
 
     resp.cookie('token', token, {
-        expires: new Date(Date.now() + process.env.COOKIE_EXP * 24 * 60 * 60 * 1000),
-        httpOnly: true, sameSite: 'none', secure: true,
+        expires: new Date(Date.now() + process.env.COOKIE_EXP * 24 * 60 * 60 * 1000), 
+        httpOnly: true, sameSite: 'none', secure: true, path: '/',
     })
         .status(statusCode).json({ success: true, role: user.role, message: `Welcome ${user?.name?.toUpperCase()}...` });
-    // .json({ success: true, role: user.role, token });
 };
 
 module.exports = sendToken;
