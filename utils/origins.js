a14b9fad3ea119442c644a51cc98aadabf1ab897
vaibhav <vaibhavallewar3@gmail.com>
2025-05-21 19:33:09 +0530

    add both sides origins in colleges.

--- utils/origins.js
+++ utils/origins.js
@@ -4,14 +4,20 @@ import Credential from "../models/credential.js";
 
 const getOrigins = async () => {
     try {
+        let origins = [];
         const colleges = await College.findAll({
             where: { status: true }, attributes: ['id', 'name', 'reg_no'],
             include: [{ model: Credential, as: 'credential', attributes: ['id', 'back_host_url', 'front_host_url'] }]
         });
 
-        const origins = colleges?.map(item => {
-            return item?.credential?.front_host_url
+        const host_urls = colleges?.map(item => {
+            return { front_host_url: item?.credential?.front_host_url, back_host_url: item?.credential?.back_host_url };
         });
+
+        host_urls?.map(item => {
+            origins.push(item.front_host_url, item.back_host_url);
+        });
+
         console.log(origins);
         return origins;
     } catch (error) {
