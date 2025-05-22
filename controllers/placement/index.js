6a2c02803add370b5350e1d07e6ed1b5901fd6a8
vaibhav <vaibhavallewar3@gmail.com>
2025-05-22 12:20:06 +0530

    add console method in placement opts.

--- controllers/placement/index.js
+++ controllers/placement/index.js
@@ -238,17 +238,22 @@ export const editPlacement = TryCatch(async (req, resp, next) => {
 // OPTIONS FOR CREATE PLACEMENT
 export const getPlaceOptions = TryCatch(async (req, resp, next) => {
 
-    const [skillPromise, companyPromise] = await Promise.all([
-        fetch(process.env.SUPER_SERVER + '/v1/master/skill/opts'),
-        fetch(process.env.SUPER_SERVER + '/v1/master/company/opts'),
-    ]);
-    console.log(JSON.stringify({ skillPromise, companyPromise, msg:['HELLO'] }));
-    const [statuses, drives, { skills }, { companies }] = await Promise.all([
-        getPlaceStatusOpts(), getPlacePositionOpts(), skillPromise.json(), companyPromise.json(),
-    ]);
+    // const [skillPromise, companyPromise] = await Promise.all([
+    //     fetch(process.env.SUPER_SERVER + '/v1/master/skill/opts'),
+    //     fetch(process.env.SUPER_SERVER + '/v1/master/company/opts'),
+    // ]);
+    // const [statuses, drives, { skills }, { companies }] = await Promise.all([
+    //     getPlaceStatusOpts(), getPlacePositionOpts(), skillPromise.json(), companyPromise.json(),
+    // ]);
 
-    const place_options = { statuses, drives, skills, companies, };
-    resp.status(200).json({ success: true, place_options });
+    // const place_options = { statuses, drives, skills, companies, };
+
+    console.log('START!');
+    const skillPromise = await fetch(process.env.SUPER_SERVER + '/v1/master/skill/opts');
+    console.log('CALL PROMISE');
+    const skill_resp = await skillPromise.json();
+    console.log('RESPONSE ' + skill_resp);
+    resp.status(200).json({ success: true, skill_resp });
 });
 
 
